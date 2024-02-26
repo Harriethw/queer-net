558208206216c50e1fb37083b2ca24d4df645126
Harriet H-W <harriethw@gmail.com>
2024-02-26 13:29:08 +0000

    add thumbnail component to news

--- pages/index.js
+++ pages/index.js
@@ -18,18 +18,27 @@ export default class Home extends Component {
           <ul className="home__list">
             {news.map((piece, k) => (
               <li key={k}>
-                <h2>{piece.name}</h2>
-                <p>{piece.description}</p>
-                {piece.link && (
-                  <p>
-                    <a href={piece.link}>Find out more</a>
-                    <span
-                      className="home__list--decoration"
-                      aria-label="hidden"
-                    >
-                      &#8592;
-                    </span>
-                  </p>
+                <div className="home__box">
+                  <h2>{piece.name}</h2>
+                  <p>{piece.description}</p>
+                  {piece.link && (
+                    <p>
+                      <a href={piece.link}>Find out more</a>
+                      <span
+                        className="home__list--decoration"
+                        aria-label="hidden"
+                      >
+                        &#8592;
+                      </span>
+                    </p>
+                  )}
+                </div>
+                {piece.thumbnail && (
+                  <div className="home__thumbnail">
+                    <a href={piece.link}>
+                    <img src={piece.thumbnail}></img>
+                    </a>
+                  </div>
                 )}
               </li>
             ))}
