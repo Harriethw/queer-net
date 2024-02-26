da55fd8001aa713d71f0d9e0cd4ffbe316732026
Harriet H-W <harriethw@gmail.com>
2024-02-26 13:39:05 +0000

    add image component to About page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,16 @@
 import { Component } from "react";
-import { react as AboutContent } from '../content/about.md'
+import { attributes, react as AboutContent } from '../content/about.md'
 
 
 export default class About extends Component {
   render() {
-    
+    let { image } = attributes;
     return (
       <>
         <article>
           <h2>About</h2>
           <AboutContent />
+          { image && (<img className="image--hero" src={image}></img>) }
         </article>
       </>
     )
