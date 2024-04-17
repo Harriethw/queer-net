c4a224a42ae32db603a00a79a4de485e810e78f5
Harriet H-W <harriethw@gmail.com>
2024-04-17 12:51:39 +0100

    experiment with darker mode
    and removing events header

--- pages/index.page.js
+++ pages/index.page.js
@@ -6,47 +6,45 @@ export default class Home extends Component {
   render() {
     let { title, news } = attributes;
     return (
-      <>
-        <article>
-          <img
-            className="image__icon"
-            alt="a small line drawing of friendly robot/humanoids"
-            src="/img/illo_3.webp"
-          />
-          <h2 className="events__content">
+      <article>
+        {/* <img
+          className="image__icon"
+          alt="a small line drawing of friendly robot/humanoids"
+          src="/img/illo_3.webp"
+        /> */}
+        {/* <h2 className="events__content">
             <HomeContent />
-          </h2>
+          </h2> */}
 
-          <ul className="events__list">
-            {news.map((piece, k) => (
-              <li key={k}>
-                <div className="events__box">
-                  <h2>{piece.name}</h2>
-                  <p>{piece.description}</p>
-                  {piece.link && (
-                    <p>
-                      <a href={piece.link}>Find out more</a>
-                      <span
-                        className="events__list--decoration"
-                        aria-label="hidden"
-                      >
-                        &#8592;
-                      </span>
-                    </p>
-                  )}
-                </div>
-                {piece.thumbnail && (
-                  <div className="events__thumbnail">
-                    <a href={piece.link}>
-                      <img src={piece.thumbnail} alt={piece.altText}></img>
-                    </a>
-                  </div>
+        <ul className="events__list">
+          {news.map((piece, k) => (
+            <li key={k}>
+              <div className="events__box">
+                <h2>{piece.name}</h2>
+                <p>{piece.description}</p>
+                {piece.link && (
+                  <p>
+                    <a href={piece.link}>Find out more</a>
+                    <span
+                      className="events__list--decoration"
+                      aria-label="hidden"
+                    >
+                      &#8592;
+                    </span>
+                  </p>
                 )}
-              </li>
-            ))}
-          </ul>
-        </article>
-      </>
+              </div>
+              {piece.thumbnail && (
+                <div className="events__thumbnail">
+                  <a href={piece.link}>
+                    <img src={piece.thumbnail} alt={piece.altText}></img>
+                  </a>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </article>
     );
   }
 }
