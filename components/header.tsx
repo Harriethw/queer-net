695c2bb2e25ac1562451810dda296fb4b5919138
Harriet H-W <harriethw@gmail.com>
2024-08-30 15:03:24 +0100

    add glow

--- components/header.tsx
+++ components/header.tsx
@@ -4,19 +4,9 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__text">
-        <span className="header__brackets" aria-hidden="true">
-          &#126;
-        </span>
-      </div>
       <Link href="/">
         <h1>machine streams</h1>
       </Link>
-      <div className="header__text">
-        <span className="header__brackets" aria-hidden="true">
-          &#126;
-        </span>
-      </div>
     </div>
   );
 };
